830567bc572845e818f7de9ab6819f585381967b
raidbojaada <raid@bojaada.nl>
2015-01-17 20:23:32 +0100

    project.php
    project.php source code !

---
+++ project.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <?php include 'helpFiles/header.php'; ?>
+    </head>
+    <body>
+        <?php include 'helpFiles/navBar.php'; ?>
+        <div class="container">
+            <center> <h3> All the projects I've made !</h3> </center>
+            <hr>
+            <br>
+            <div class="row">
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+                        <center> <h4> <i class="fa fa-building-o"></i> Castle Jemeppe</h4></center>
+                        <br>
+                        <p>My colleague and I made a web-application for a Miss Norton, who bought a castle and turned it into a Hotel. We are still working on the web-app. </p>
+                        <p>We used: PhP and MySQL.</p>
+                        <br>
+                        <p>My Castle Jemeppe Web- Application:</p>
+                    
+                        <button><a href="http://kasteeljemeppe.rhenusonerosalia.com/" target="_blank"/> <i class="fa fa-building-o"></i> Look</a></button>
+        
+                    </div>
+                </div>
+                
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+                        <center> <h4> <i class="fa fa-car fa-1x"></i> TopGear</h4></center>
+                        <br>
+                        <p>In the first year of Roc Flevoland, we made a online Java web-application where you can search for cars based on brand and price. </p>
+                        <p>I used: JSP.</p>
+                        <br>
+                        <p>My TopGear Web- Application:</p>
+                    
+                        <button><a href="http://topgearopdr.appspot.com/" target="_blank"/> <i class="fa fa-car"></i> Look</a></button>
+        
+                    </div>
+                </div>
+                
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+                        <center> <h4> <i class="fa fa-steam-square fa-1x"></i> Steam Teams</h4></center>
+                        <br>
+                        <p>In the second year we made a JSP web-application, we had to make use of Java and MySQL. You can create/delete members and add/remove them from a team.  </p>
+                        
+                        <p>My Steam Teams Web- Application : Trying to put JSP online !</p>
+        
+                        <div class="progress progress-striped active">
+                            <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 75%;">
+                                <span class="sr-only">60% Complete</span>
+                            </div>
+                        </div>
+        
+                    </div>
+                </div>
+                
+            </div>
+
+            <?php include 'helpFiles/footer.php'; ?>
+        </div>
+    </body>
+</html>
